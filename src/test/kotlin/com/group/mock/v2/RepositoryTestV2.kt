94faca6b89dd6bb0f4281536514406bee7ab1489
yooniversal <iteratively@naver.com>
2024-07-19 01:21:11 +0900

    테스트 코드 추가

--- src/test/kotlin/com/group/mock/v2/RepositoryTestV2.kt
+++ src/test/kotlin/com/group/mock/v2/RepositoryTestV2.kt
@@ -44,6 +44,72 @@ class RepositoryTestV2 {
     }
 
     @Test
+    @DisplayName("여러 Beverage를 한번에 저장 및 조회할 수 있다")
+    fun `save and read Beverages`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+
+        // when
+        val savedBeverages = beverageRepository.saveAll(listOf(milk, coffee))
+
+        // then
+        beverageRepository.findAll().let { beverages ->
+            assertThat(beverages[0]).isEqualTo(savedBeverages[0])
+            assertThat(beverages[1]).isEqualTo(savedBeverages[1])
+        }
+    }
+
+    @Test
+    @DisplayName("여러 Beverage를 id로 조회할 수 있다")
+    fun `save and read Beverages by ids`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+
+        // when
+        val savedBeverages = beverageRepository.saveAll(listOf(milk, coffee))
+
+        // then
+        beverageRepository.findAllById(savedBeverages.map { it.id }).let { beverages ->
+            assertThat(beverages[0]).isEqualTo(savedBeverages[0])
+            assertThat(beverages[1]).isEqualTo(savedBeverages[1])
+        }
+    }
+
+    @Test
+    @DisplayName("여러 Beverage를 한번에 저장 및 조회할 수 있다")
+    fun `verify the Beverage by id`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val beverage = Beverage(name = "milk", food = food, place = place)
+        beverageRepository.save(beverage)
+
+        // when & then
+        assertThat(beverageRepository.existsById(1L)).isTrue
+    }
+
+    @Test
+    @DisplayName("저장된 Beverage의 수를 알 수 있다")
+    fun `read count of Beverages`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+        val tea = Beverage(name = "tea", food = food, place = place)
+        beverageRepository.saveAll(listOf(milk, coffee, tea))
+
+        // when & then
+        assertThat(beverageRepository.count()).isEqualTo(3)
+    }
+
+    @Test
     @DisplayName("Beverage를 수정할 수 있다")
     fun `modify a Beverage`() {
         // given
@@ -83,6 +149,42 @@ class RepositoryTestV2 {
     }
 
     @Test
+    @DisplayName("저장된 Beverage를 모두 삭제할 수 있다")
+    fun `delete all of Beverages`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+        val tea = Beverage(name = "tea", food = food, place = place)
+        beverageRepository.saveAll(listOf(milk, coffee, tea))
+
+        // when
+        beverageRepository.deleteAll()
+
+        // then
+        assertThat(beverageRepository.count()).isZero
+    }
+
+    @Test
+    @DisplayName("여러 Beverage를 id로 삭제할 수 있다")
+    fun `delete multiple of Beverages by ids`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+        val tea = Beverage(name = "tea", food = food, place = place)
+        val savedIds = beverageRepository.saveAll(listOf(milk, coffee, tea)).map { it.id }
+
+        // when
+        beverageRepository.deleteAllById(savedIds)
+
+        // then
+        assertThat(beverageRepository.count()).isZero
+    }
+
+    @Test
     @DisplayName("Beverage를 entity로 삭제할 수 있다")
     fun `delete Beverage by entity`() {
         // given
@@ -98,6 +200,24 @@ class RepositoryTestV2 {
         beverageRepository.findByIdOrNull(savedBeverage.id).also { beverage ->
             assertThat(beverage).isNull()
         }
+    }
+
+    @Test
+    @DisplayName("여러 Beverage를 entity로 삭제할 수 있다")
+    fun `delete multiple of Beverages by entities`() {
+        // given
+        val food = Food(name = "bread")
+        val place = Place(name = "bakery")
+        val milk = Beverage(name = "milk", food = food, place = place)
+        val coffee = Beverage(name = "coffee", food = food, place = place)
+        val tea = Beverage(name = "tea", food = food, place = place)
+        val savedBeverages = beverageRepository.saveAll(listOf(milk, coffee, tea))
+
+        // when
+        beverageRepository.deleteAllInBatch(savedBeverages)
+
+        // then
+        assertThat(beverageRepository.count()).isZero
     }
 
     @Test
